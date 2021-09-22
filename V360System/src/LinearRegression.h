695ec0948b844fa8f91bcdbfda164226bf19a8be
Ehab Ghabashneh <eghabash@Ehabs-MacBook-Pro.local>
2021-09-22 13:59:15 -0400

    [system][LinearRegression] clean and add to makefiles

--- V360System/src/LinearRegression.h
+++ V360System/src/LinearRegression.h
@@ -1,4 +1,5 @@
-// This is adapted from: https://www.analyticsvidhya.com/blog/2020/04/machine-learning-using-c-linear-logistic-regression/
+// This is adapted from:
+// https://www.analyticsvidhya.com/blog/2020/04/machine-learning-using-c-linear-logistic-regression/
 
 /*
  * LinearRegression.h
@@ -6,53 +7,48 @@
  *  Created on: Sep 15, 2021
  *      Author: cbothra
  */
- 
-#include<bits/stdc++.h> // header file for all c++ libraries
-#include<vector>
 
-using namespace std;   // stdout library for printing values 
-
+#include <utility>
+#include <vector>
 
 class LinearRegression {
+ public:
+  /* this custom sort function is defined to
+  sort on basis of min absolute value or error*/
 
-public:
+  bool custom_sort(double a, double b) {
+    double a1 = abs(a - 0);
+    double b1 = abs(b - 0);
+    return a1 < b1;
+  }
 
-	/* this custom sort function is defined to 
-	sort on basis of min absolute value or error*/
+  static constexpr size_t HW = 13;
+  static constexpr size_t PW = 25;
+  static constexpr float lower_bound = 1e-5;
 
-	bool custom_sort(double a, double b) {
-		double  a1=abs(a-0);
-		double  b1=abs(b-0);
-		return a1<b1;
-	}
+  size_t hw_{HW};
+  size_t pw_{PW};
 
-	static constexpr size_t HW = 13;
-	static constexpr size_t PW = 25;
-	static constexpr float lower_bound = 1e-5;
-	
-	size_t hw_ {HW};
-	size_t pw_ {PW};
+  float yawInput[HW];
+  float pitchInput[HW];
+  float timeSampleInput[HW];
 
-	float yawInput[HW];
-	float pitchInput[HW];
-	float timeSampleInput[HW];
+  std::vector<float> yawError;  // for storing the yaw error values
+  float yawErr;                 // for calculating yaw error on each stage
+  float yawB0 = 0;              // initializing yaw b0
+  float yawB1 = 0;              // initializing yaw b1
+  float yawAlpha = 0.01;        // initializing our learning rate
 
-	vector<float>yawError; // for storing the yaw error values
-	float yawErr;    // for calculating yaw error on each stage
-	float yawB0 = 0; // initializing yaw b0
-	float yawB1 = 0; // initializing yaw b1
-	float yawAlpha = 0.01; // initializing our learning rate
+  std::vector<float> pitchError;  // for storing the pitch error values
+  float pitchErr;                 // for calculating pitch error on each stage
+  float pitchB0 = 0.0;            // initializing pitch b0
+  float pitchB1 = 0.0;            // initializing pitch b1
+  float pitchAlpha = 0.01;        // initializing our learning rate
 
-	vector<float>pitchError; // for storing the pitch error values
-	float pitchErr;    // for calculating pitch error on each stage
-	float pitchB0 = 0.0; // initializing pitch b0
-	float pitchB1 = 0.0; // initializing pitch b1
-	float pitchAlpha = 0.01; // initializing our learning rate
+  float e = 2.71828;
 
-	float  e = 2.71828;
-	
+  std::vector<std::pair<float, float>> lrPredictions;
 
-	std::vector<std::pair<float, float>> lrPredictions;
-	
-	std::vector<std::pair<float, float>> Model(std::vector<std::pair<float, float>> input, int length);
+  std::vector<std::pair<float, float>> Model(
+      std::vector<std::pair<float, float>> input, int length);
 };
