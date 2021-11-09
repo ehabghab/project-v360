bcda83829e8bd0d00587b140aefc6b8b9b985cbf
Ehab <eghabash@purdue.edu>
2021-11-09 11:59:05 -0500

    [automate] update script.

--- automate/automate.py
+++ automate/automate.py
@@ -70,9 +70,9 @@ def run_server_and_get_ip(tracefile):
 
 def main():
 
-	user_trace_dir = "/home/ehab/Desktop/Project-V360/split/"
+	user_trace_dir = "../split/"
 	tile_size ="quality_tile_sizes.txt"
-	user_tile_per_frame_traces = ["tiles_per_frame_synthetic_user_1.txt","tiles_per_frame_user_3.txt","tiles_per_frame_user_13.txt"]#,"tiles_per_frame_user_29.txt"]
+	user_tile_per_frame_traces = ["tiles_per_frame_user_13.txt"]#["tiles_per_frame_synthetic_user_1.txt","tiles_per_frame_user_3.txt","tiles_per_frame_user_13.txt"]#,"tiles_per_frame_user_29.txt"]
 	#user_vp_corr_per_frame_traces = ["vp_corr_per_frame_user_3.txt"S,"vp_corr_per_frame_user_13.txt","vp_corr_per_frame_user_29.txt"]
 	bw_trace_dir = "/home/ehab/Desktop/Project-V360/automate/"        
 	bw_traces = ["trace_mahimahi_20.33mbps.txt", "trace_mahimahi_6.2mbps.txt","trace_mahimahi_3.1mbps.txt"]
